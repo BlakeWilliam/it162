c16fcb948f2572b538078d72697e5e56bd2d5421
BlakeWilliam <71867420+BlakeWilliam@users.noreply.github.com>
2021-01-15 21:51:45 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+ <?php include "includes/header.php";?>
+<!-- START LEFT COL -->
+<section>
+ <h2 class="pageID">Welcome</h2>
+
+<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+ <img src="images/large.jpg" class="desktop" alt="picture of Blake" />
+ <img src="images/small.jpg" class="phone" alt="picture of Blake" />
+ <p>Hello there!</p>
+ <p>I'm Blake and I am currently in the Web Development program at Seattle Central College. For many years I've worked in the Restaurant/Bar industry as well as having owned and operated a business of my own. In the current economic climate, I've decided to make a change in carreer paths, and what better way than pursuing something I've always had an interest in. In the future I hope to combine strong web design skills with development and pursue a front-end development job. </p>
+ <p>I've always been drawn to technology and the languages surrounding it. I atteneded a tech/computer science program through my school as an elementary school kid. Through high school in the late 90's and early 2000's I taught myself the basics of HTML, CSS, and PHP and the basics of photoshop. In turn I built websites and entered graphic design challenges as a hobby.  </p>
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+ <h2>Right Column</h2>
+ <img src="images/medium.jpg" class="tablet" alt="picture of Blake" />
+ <p>In my free time I absolutely love the outdoors. Living in the PNW for the last 17 years has given me the opprotunity to hike some of the most beautiful trails anywhere in the world. Camping, kayaking, swimming, and just hanging out outside are all definite perks of our beaustiful state.  </p>
+ <p>I also enjoy cooking. I started cooking as a kid and haven't looked back. Learning new cuisines and how to use exotic spices are my favorite parts. Sharing a meal with friends is also one of my favorite things to do.</p>
+</aside>
+<!-- END RIGHT COL -->
+<?php include "includes/footer.php";?>
