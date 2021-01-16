78b630657134d3caded4b85fc2f89b39b2568641
BlakeWilliam <71867420+BlakeWilliam@users.noreply.github.com>
2021-01-16 12:40:20 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
  <?php include "includes/header.php";?>
-<!-- START LEFT COL -->
-<section>
- <h2 class="pageID">Welcome</h2>
 
 <!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
  <img src="images/large.jpg" class="desktop" alt="picture of Blake" />
@@ -16,8 +13,9 @@
 <aside>
  <h2>Right Column</h2>
  <img src="images/medium.jpg" class="tablet" alt="picture of Blake" />
- <p>In my free time I absolutely love the outdoors. Living in the PNW for the last 17 years has given me the opprotunity to hike some of the most beautiful trails anywhere in the world. Camping, kayaking, swimming, and just hanging out outside are all definite perks of our beaustiful state.  </p>
+ <p>In my free time I absolutely love the outdoors. Living in the PNW for the last 17 years has given me the opprotunity to hike some of the most beautiful trails anywhere in the world. Camping, kayaking, swimming, and just hanging out outside are all definite perks of living in our beautiful state.  </p>
  <p>I also enjoy cooking. I started cooking as a kid and haven't looked back. Learning new cuisines and how to use exotic spices are my favorite parts. Sharing a meal with friends is also one of my favorite things to do.</p>
 </aside>
 <!-- END RIGHT COL -->
+
 <?php include "includes/footer.php";?>
