cb4a637446ef812b3de12445709d7b073d47511b
BlakeWilliam <71867420+BlakeWilliam@users.noreply.github.com>
2021-03-03 15:04:41 -0800

    Upload aia.php ( & updates to css files)

--- aia.php
+++ aia.php
@@ -1,15 +1,146 @@
  <?php include "includes/header.php";?>
-    <p>Future home of the AIA Section!</p>
-    <p>I'm baby hexagon keffiyeh williamsburg quis irure ipsum godard. Eu bespoke neutra before they sold out mlkshk truffaut sunt readymade kinfolk ethical put a bird on it godard jianbing cardigan. Prism man bun echo park, authentic whatever butcher shabby chic subway tile aliquip before they sold out raclette dreamcatcher tbh cliche labore. DIY chillwave beard, mlkshk tote bag raclette master cleanse trust fund la croix everyday carry etsy cold-pressed adipisicing PBR&B. Chicharrones unicorn sartorial chambray.
-    </p>
 </section>
-<!-- END LEFT COL -->
+<div>
+<table class="padding client-info">
+    <tr>
+        <th>Client Name & Final Decisions</th>
+        <td>Sara Bathan</td>
+    </tr>
+    <tr>
+        <th>Clients Business (website name)</th>
+        <td>Sara Bathan Designs</td>
+    </tr>
+    <tr>
+        <th>Redesign or brand new website?</th>
+        <td>Brand New</td>
+    </tr>
+    <tr>
+        <th>Type of website</th>
+        <td>Personal Branding & Identity / Portfolio</td>
+    </tr>
+    <tr>
+        <th>Client goals</th>
+        <td>As a new graphic design graduate, she would like to have an internet presence to showcase her work and present her personal brand. Use to gain information on potential clients and gain leads for new work.</td>
+    </tr>
+</table><br>
+<table class="padding aiaprofile">
+    <tr>
+        <th>Audience</th>
+        <th>Issues</th>
+        <th>Approach</th>
+    </tr>
+    <tr>
+        <td><span>Audience:<br><br></span>
+            <strong>Freelance Web Developers:</strong><br>This is a person who has coding/design skills when it comes to building and maintaining websites/web apps. They lean heavily towards coding than on actual graphics and art. They will often need to collaborate with a graphic designer to complete the visual/art aspects of a project’s build.<br><br> These people are extremely web savvy and generally are in the early 20’s-40’s. They are very independent working, and generally know what they want from others they work with.<br><br>These people could be looking for temporary collaborators for single projects or to build a 1-on-1 collaborative relationship that spans multiple projects and more. Generally they already have their own clients and need assistance in some form with visual aspects.
+        </td>
+        <td>
+            <ul>
+                <lh>Issues</lh>
+                <li>Would like to see examples of work before deciding if she’s a fit for their client’s project.</li>
+                <li>They may also need to act as a middle man to their client if the client is in need of more in-depth services like branding. So accessible examples of past work need to be passed on.</li>
+                <li>Easy contact information</li>
+                <li>They will want to know what her skill sets are and what programs, file types of graphics she can produce.</li>
+            </ul>
+        </td>
+        <td>
+            <ul>
+                <lh>Approach</lh>
+                <li>Easy to navigate, clean design to help accentuate Sara’s work and not distract a freelancer from focusing on other elements that are not her own.</li>
+                <li>Clean, clear examples of her work.</li>
+                <li>Contact page with relevant information.</li>
+                <li>List of qualifications.</li>
+            </ul>
+        </td>
+    </tr>
 
-<!-- START RIGHT COL -->
-<aside>
-    <h2>Right Column</h2>
-    <p>Readymade tofu pariatur squid dolore tacos chia trust fund gochujang waistcoat freegan selfies. Tumblr meh dreamcatcher, quis intelligentsia small batch vexillologist before they sold out. Celiac stumptown dolore hashtag, twee typewriter magna lorem paleo non. Aute qui williamsburg pinterest actually YOLO flannel tbh pour-over. Ennui proident cupidatat seitan. Polaroid actually kombucha cillum irure waistcoat</p>
-</aside>
-<!-- END RIGHT COL -->
+    <tr>
+        <th>Audience</th>
+        <th>Issues</th>
+        <th>Approach</th>
+    </tr>
+    <tr>
+        <td>
+            <span>Audience:<br><br></span>
+            <strong>Company Representatives:</strong><br>These people will be representatives of a company, which could be a small start-up or more of a traditional large company. This could possibly be a recruiter.<br><br>Generally these people will be someone in their later 20’s-50’s. They will be pretty well versed in general internet/computer skills but may not necessarily have graphic design skills.<br><br>In the event of a need for graphic design on a project they currently have going, they may need some additional graphic design support and will reach out to qualified people with web presences.
+        </td>
+        <td>
+            <ul>
+                <lh>Issues</lh>
+                <li>They will be very interested in her past work.</li>
+                <li>TThey will want to know about her skills and the types of files she works with.</li>
+                <li>They are interested in if she works on specific projects (i.e print, web, packaging, etc).</li>
+                <li>They will want an easy way to access contact information.</li>
+            </ul>
+        </td>
+        <td>
+            <ul>
+                <lh>Approach</lh>
+                <li>List of qualifications.</li>
+                <li>Examples of past work.</li>
+                <li>Easy and accessible contact information.</li>
+            </ul>
+        </td>
+    </tr>
+    <tr>
+        <th>Audience</th>
+        <th>Issues</th>
+        <th>Approach</th>
+    </tr>
+    <tr>
+        <td>
+    <span>Audience:<br><br></span>
+            <strong>New Business Owners:</strong><br>New business owners or ones looking to start their own businesses may be looking for someone to help create their own brand identity.<br><br>This group is possibly endless in the range of ages and skill sets. But a good place to strive for is to think they are probably in an unrelated IT/Art/Graphic Design industry. They may not have or want to have in-depth skills of web/tech. They are masters of their own domains.<br><br>They will seek out someone with a strong web presence and personal branding of their own for help. 
+        </td>
+        <td>
+            <ul>
+                <lh>Issues</lh>
+                <li>As they are a potential direct client, they will want to know a bit more about her as a person.</li>
+                <li>They will want to know what direct services she can provide them with.Does she do inclusive design packages? Will she work on just a specific thing for them.</li>
+                <li>They will want to have an easy way to have a direct line of communication.</li>
+                <li>They will want to know the costs of services.</li>
+            </ul>
+        </td>
+        <td>
+            <ul>
+                <lh>Approach</lh>
+                <li>Examples of past work.</li>
+                <li>Display of a strong, clear sense of self-branding to gain trust for her work.</li>
+                <li>While they may want to know prices - having a list of services she can provide for new business owners, and a direct way to contact her for a quote is appropriate as each project will vary.</li>
+                <li>Examples of past client work relevant to branding for businesses.</li>
+            </ul>
+        </td>
+    </tr>
 
+    <tr>
+        <th>Audience</th>
+        <th>Issues</th>
+        <th>Approach</th>
+    </tr>
+    <tr>
+        <td>
+            <span>Audience:<br><br></span>
+            <strong>Other Graphic Design/Art/IT industry Professionals:</strong><br>These people are professionals, or aspiring professionals in the Graphic Design industry, or a related industry. They are also probably ranging in ages from early 20’s up to their 50’s. Very tech savvy and enthusiastic to expand their networks through web/tech interfaces.<br><br>They are looking for quality people to network with. They may have received info from a friend or colleague to reach out to a possible new contact. Or they may want to gain more information about Sara’s work after meeting her This will help in the event that they know someone in need of a graphic designer, someone in their network will be able to pass along contact information.
+        </td>
+        <td>
+            <ul>
+                <lh>Issues</lh>
+                <li>They are very interested in her as a person so will be interested in a bio of some kind.</li>
+                <li>They will want to know her skill set, proficiencies, and education background.</li>
+                <li>They want to have links to social media accounts to have more meaningful interactions.</li>
+                <li>They may want to know more about her existing network, and who she may have worked with in the past.</li>
+            </ul>
+        </td>
+        <td>
+            <ul>
+                <lh>Approach</lh>
+                <li>A strong bio page featuring information of her background, interests and qualifications.</li>
+                <li>A contact page that includes links to social media access to further engagement.</li>
+                <li>Clean, professional styling.</li>
+            </ul>
+        </td>
+    </tr>
+</table><br>
+
+  
+</div>
 <?php include "includes/footer.php";?>
