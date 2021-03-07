3e75206a8d5e41728db92cf0b5897428fa5128ee
BlakeWilliam <71867420+BlakeWilliam@users.noreply.github.com>
2021-03-07 02:14:05 -0800

    update for flowchart.php

--- flowchart.php
+++ flowchart.php
@@ -1,14 +1,4 @@
 <?php include "includes/header.php";?>
-<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
- <p>Future home of the Flowchart Section!</p>
-</section>
-<!-- END LEFT COL -->
-
-<!-- START RIGHT COL -->
-<aside>
- <h2>Some Flow</h2>
- <p>I'm baby meggings tbh crucifix, prism etsy bicycle rights exercitation jean shorts sriracha raw denim ullamco proident nulla copper mug. 8-bit actually readymade edison bulb consectetur, chambray 90's typewriter quis 3 wolf moon kale chips banjo ugh nisi jianbing. Organic poutine shaman, glossier esse succulents ugh migas. Anim iceland occupy, trust fund readymade echo park iPhone cronut vice poutine esse do. Tempor offal exercitation sriracha man bun fingerstache. Kickstarter plaid tacos, raclette hammock id et listicle palo santo twee marfa kitsch forage ullamco.</p>
-</aside>
-<!-- END RIGHT COL -->
+Go to the flowchart and wireframe document by clicking <a href="https://docs.google.com/document/d/14yVcPlMCxrHXT6ThDPTCoEO_RHxRRIT4CBuTxD4qG10/edit?usp=sharing" target="_blank">here</a>.
 
 <?php include "includes/footer.php";?>
